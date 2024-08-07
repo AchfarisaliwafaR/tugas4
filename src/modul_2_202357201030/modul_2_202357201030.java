72f3468b87ad7561f414e79ad802bc14241681c2
TJA Tech <TJA Tech@DESKTOP-JC7UP1Q>
2024-08-07 18:23:34 +0700

    first

---
+++ src/modul_2_202357201030/modul_2_202357201030.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modul_2_202357201030;
+
+import java.lang.reflect.Array;
+
+
+/**
+ *
+ * @author TJA Tech
+ */
+public class modul_2_202357201030 {
+    public static void main(String[] args) {
+        System.out.println("A FARIS ALI WAFA R");
+        main(null);
+        String.format(null, (Object) args);
+        
+    }
+    
+}
